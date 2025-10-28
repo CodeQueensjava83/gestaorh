24a69cfb4d02bfb5b70f2e33815ee19ffcac8534
Milena <milenasvitras@gmail.com>
2025-10-28 14:42:43 -0300

    :sparkles: Security classe UserDetailsImpl e UserDetailsServiceImpl e Classe UsuarioService

---
+++ src/main/java/com/generation/gestaorh/security/UserDetailsServiceImpl.java
@@ -0,0 +1,42 @@
+package com.generation.gestaorh.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.generation.gestaorh.model.Usuario;
+import com.generation.gestaorh.repository.UsuarioRepository;
+ 
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+
+	private UsuarioRepository usuarioRepository;
+ 
+	@Override
+
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+ 
+		if (username == null || username.trim().isEmpty()) {
+			throw new UsernameNotFoundException("Usuário (e-mail) não pode ser vazio");
+
+		}
+
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(username);
+ 
+		if (usuario.isPresent()) {
+			return new UserDetailsImpl(usuario.get());
+
+		}else {
+			throw new UsernameNotFoundException("Usuário não encontrado: " + username);
+
+		}
+
+	}
+
+}
