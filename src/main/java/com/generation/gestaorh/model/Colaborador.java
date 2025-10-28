5eecb47d5c98d2eae72a0a3fb9738f9cc93ba5e8
carina <carinabentlin@gmail.com>
2025-10-28 14:16:30 -0300

    :sparkles: Cria feature Colaborador

--- src/main/java/com/generation/gestaorh/model/Colaborador.java
+++ src/main/java/com/generation/gestaorh/model/Colaborador.java
@@ -1,5 +1,41 @@
 package com.generation.gestaorh.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name = "tb_colaboradores")
 public class Colaborador {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "O cargo é obrigatório")
+    private String cargo;
+
+    @NotBlank(message = "O setor é obrigatório")
+    private String setor;
+
+    @NotNull(message = "O salário é obrigatório")
+    private Double salario;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
+
+    public String getSetor() { return setor; }
+    public void setSetor(String setor) { this.setor = setor; }
+
+    public Double getSalario() { return salario; }
+    public void setSalario(Double salario) { this.salario = salario; }
 }
--- src/main/java/com/generation/gestaorh/repository/ColaboradorRepository.java
+++ src/main/java/com/generation/gestaorh/repository/ColaboradorRepository.java
@@ -1,5 +1,15 @@
 package com.generation.gestaorh.repository;
 
-public interface ColaboradorRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.gestaorh.model.Colaborador;
+
+@Repository
+public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
+
+    List<Colaborador> findAllByNomeContainingIgnoreCase(String nome);
 
 }
