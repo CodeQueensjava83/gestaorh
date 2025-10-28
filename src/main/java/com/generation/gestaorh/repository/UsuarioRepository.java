48fe469188c25c35b59b3c4a2894bff293f1294b
Maria <mjuliamedeiros.2016@gmail.com>
2025-10-28 17:23:24 -0300

    Usuario e UsuarioLogin

--- src/main/java/com/generation/gestaorh/repository/UsuarioRepository.java
+++ src/main/java/com/generation/gestaorh/repository/UsuarioRepository.java
@@ -1,5 +1,16 @@
 package com.generation.gestaorh.repository;
 
-public interface UsuarioRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.gestaorh.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	public List<Usuario> findAllByUsuarioContainingIgnoreCase(String usuario);
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+	
 }
