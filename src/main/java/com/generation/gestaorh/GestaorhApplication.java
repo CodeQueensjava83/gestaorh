b4f47fa26b88e3b66a47d93a65a9f05a6ae69f19
MyListon <myliston@gmail.com>
2025-10-30 12:21:37 -0300

    :rocket: Ajustes Deploy

--- src/main/java/com/generation/gestaorh/GestaorhApplication.java
+++ src/main/java/com/generation/gestaorh/GestaorhApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestaorhApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(GestaorhApplication.class, args);
 	}
 
